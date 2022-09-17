a0c27081ec16442989b6fea1019faf88075801d9
Vincent Beaulieu <37360448+vincbeaulieu@users.noreply.github.com>
2022-09-17 19:22:25 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,27 @@
+# ref: https://stackoverflow.com/questions/714063/importing-modules-from-parent-folder
+
+from setuptools import setup, find_packages
+
+setup(name='HDV-LIG14', version='1.0', packages=find_packages())
+
+
+##
+# ref: https://sourabhbajaj.com/mac-setup/Python/virtualenv.html
+
+# To activate the virtual environment:
+# source .venv/bin/activate
+
+# To leave the virtual environment:
+# deactivate
+
+
+##
+# ref: https://stackoverflow.com/questions/714063/importing-modules-from-parent-folder
+
+# In the root directory, run
+# pip install -e . (note the dot, it stands for "current directory")
+
+#numpy
+#pandas
+
+
