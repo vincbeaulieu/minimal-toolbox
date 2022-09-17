1171c0d16ffaec227377bc2a329017cf8bb516d5
Vincent Beaulieu <37360448+vincbeaulieu@users.noreply.github.com>
2022-09-17 19:26:48 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 # ref: https://stackoverflow.com/questions/714063/importing-modules-from-parent-folder
 
 from setuptools import setup, find_packages
-
-setup(name='HDV-LIG14', version='1.0', packages=find_packages())
+setup(name='minimal-toobox', version='1.0', packages=find_packages())
 
 
 ##
